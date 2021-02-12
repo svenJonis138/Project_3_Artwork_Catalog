88967fbf053b6ce3eca77dee729bf84f596bcfb7
mania138 <123@fakestreet.com>
2021-02-12 14:16:00 -0600

    created artist and artwork classes

---
+++ artist.py
@@ -0,0 +1,6 @@
+class Artist:
+    def __init__(self, artist_name, email, id=None):
+        self.artist_name = artist_name
+        self.email = email
+        self.id = id
+
---
+++ artwork.py
@@ -0,0 +1,6 @@
+class Artwork:
+    def __init__(self, artist_name, art_work_name, price, available):
+        self.artist_name = artist_name
+        self.art_work_name = art_work_name
+        self.price = price
+        self.available = available
