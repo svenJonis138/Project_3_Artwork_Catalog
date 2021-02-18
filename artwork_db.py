1fb76cdecc2b1d99568d0645ac919ae9d3422b4f
mania138 <123@fakestreet.com>
2021-02-18 02:57:26 -0600

    test db is def working, started writing tests for artist related sql functions. so far all are passing

--- artwork_db.py
+++ artwork_db.py
@@ -1,4 +1,3 @@
-
 import sqlite3
 import artist
 from config import db_path
@@ -25,7 +24,7 @@ def add_artist(new_artist):
     insert_sql = 'INSERT INTO artists (artist_name, email) VALUES (?, ?)'
     try:
         with sqlite3.connect(db_path) as conn:
-            res = conn.execute(insert_sql, (new_artist.artist_name, new_artist.artist.email))
+            res = conn.execute(insert_sql, (new_artist.artist_name, new_artist.email))
             new_id = res.lastrowid  # Get the ID of the new row in the table
             artist.id = new_id  # Set this artist's ID
         conn.close()
@@ -37,12 +36,21 @@ def get_all_artists():
     """Displays all artists currently in db"""
     con = sqlite3.connect(db_path)
     artists_cursor = con.execute('SELECT * FROM artists')
-    artists = [artist.Artist(*row) for row in artists_cursor.fetchall() ]
+    artists = [artist.Artist(*row) for row in artists_cursor.fetchall()]
     con.close()
     return artists
+
+
+def delete_artist(artist_to_delete):
+    """deletes specified artist (must match spelling)"""
+    try:
+        with sqlite3.connect(db_path) as conn:
+            conn.execute('DELETE FROM artists WHERE artist_name LIKE ? COLLATE NOCASE', (artist_to_delete.artist_name,))
+        conn.close()
+    except sqlite3.IntegrityError as e:
+        raise ArtDbError(f'Error - this artist was not found in the database. {artist}') from e
 
 
 class ArtDbError(Exception):
     """for any db errors"""
     pass
-
--- test/test_artist_db.py
+++ test/test_artist_db.py
@@ -5,9 +5,10 @@ from unittest import TestCase
 import os
 
 from artist import Artist
-# from config import db_path
+
 import config
-db_path = os.path.join('database', 'test_artwork_catalog.db')
+
+db_path = os.path.join('test_database', 'test_artwork_catalog.db')
 config.db_path = db_path
 import artwork_db
 
@@ -21,14 +22,43 @@ class TestArtWorkDB(TestCase):
         conn.commit()
         conn.close()
 
-    def test_add_artist(self):
-        print('database being used is ' + db_path)
+    def test_get_all_artists_empty_db(self):
+        artwork_db.get_all_artists()
+        self.assertRaises(artwork_db.ArtDbError)
 
-        test_name1 = 'Sven Jonis'
-        test_email1 = 'sven@fake.org'
-        test_artist1 = Artist(test_name1, test_email1)
-        print(test_artist1)
-        artwork_db.add_artist(test_artist1)
+    def test_add_artist(self):
+        a1 = Artist('Sven Jonis', 'sven@fake.com')
+        artwork_db.add_artist(a1)
+
+    def test_get_all_artists_one_artist(self):
+        a1 = Artist('Sven Jonis', 'sven@fake.com')
+        artwork_db.add_artist(a1)
+        counter = 0
+        result = artwork_db.get_all_artists()
+        for artist in result:
+            print(artist)
+            counter += 1
+        self.assertEquals(1, counter)
+
+    def test_artist_already_exists(self):
+        a1 = Artist('Sven Jonis', 'sven@fake.com')
+        artwork_db.add_artist(a1)
+        self.assertRaises(artwork_db.ArtDbError)
+
+    def test_artist_no_name(self):
+        a1 = Artist('', 'sven@fake.com')
+        artwork_db.add_artist(a1)
+        self.assertRaises(artwork_db.ArtDbError)
+
+    def test_delete_artist_in_db(self):
+        a1 = Artist('', 'sven@fake.com')
+        artwork_db.add_artist(a1)
+        artwork_db.delete_artist(a1)
+
+    def test_delete_artist_not_in_db(self):
+        a1 = Artist('Captain NoFun', 'thecap@nofun.com')
+        artwork_db.delete_artist(a1)
+        self.assertRaises(artwork_db.ArtDbError)
 
 
 if __name__ == '__main__':
